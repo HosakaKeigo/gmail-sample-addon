7a894be264841c5dc0c8359c395061b319ca5193
HosakaKeigo <gomagoma313@gmail.com>
2024-09-16 00:06:28 +0900

    [add] HomePageCardの修正

--- src/cards/HomePage.ts
+++ src/cards/HomePage.ts
@@ -1,12 +1,13 @@
 const HomePageCard = (): GoogleAppsScript.Card_Service.Card => {
   return CardService.newCardBuilder()
-    .setHeader(CardService.newCardHeader().setTitle("Home Page"))
+    .setHeader(CardService.newCardHeader().setTitle("メール要約AIアシスタント"))
     .addSection(
       CardService.newCardSection().addWidget(
         CardService.newDecoratedText()
-          .setTopLabel("Welcome")
-          .setText("Hello, World!"),
+          .setTopLabel("使い方")
+          .setText("アドオンを起動した状態でメールを選択してください。その後、「要約」ボタンをクリックするとスレッドの内容を要約します。")
+          .setWrapText(true), // テキストの折り返しを許可
       ),
     )
-    .build(); // buildを忘れないこと。
+    .build();
 };
